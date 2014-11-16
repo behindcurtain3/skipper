df6a1f3ceae2e7a51e14e7e0e4f38aafa000525e
Justin Brown <behindcurtain3@gmail.com>
2014-11-15 21:49:12 -0800

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skipper::Application.config.session_store :cookie_store, key: '_skipper_session'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Skipper::Application.routes.draw do
+  root "welcome#index"
+end
