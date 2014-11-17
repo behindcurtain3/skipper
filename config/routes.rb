94ab62ecc5d65d3c5ee3dfc673bab064ba7801ef
Justin Brown <behindcurtain3@gmail.com>
2014-11-16 22:52:28 -0800

    Added devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Skipper::Application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get "signin", to: "devise/sessions#new"
+    get "newb", to: "devise/registrations#new"
+  end
+
   root "welcome#index"
 end
