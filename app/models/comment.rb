089c7f5759e2e5a42f678506db49f429e8672059
Justin Brown <behindcurtain3@gmail.com>
2014-11-24 23:18:34 -0800

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -25,6 +25,7 @@ class Comment < ActiveRecord::Base
   validates :content, presence: true
   validates :user_id, presence: true
   validates :post_id, presence: true
+	validates :token, presence: true
 
   private
 
